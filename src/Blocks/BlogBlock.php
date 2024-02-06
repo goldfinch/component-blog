d8815fba6c7848c2442a352333bdcde56b35703a
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-02-07 08:27:28 +1300

    WIP

--- src/Blocks/BlogBlock.php
+++ src/Blocks/BlogBlock.php
@@ -2,18 +2,13 @@
 
 namespace Goldfinch\Component\Blog\Blocks;
 
-use Goldfinch\Fielder\Fielder;
-use Goldfinch\Mill\Traits\Millable;
-use Goldfinch\Fielder\Traits\FielderTrait;
-use DNADesign\Elemental\Models\BaseElement;
+use Goldfinch\Blocks\Models\BlockElement;
 use Goldfinch\Component\Blog\Models\Nest\BlogTag;
 use Goldfinch\Component\Blog\Models\Nest\BlogItem;
 use Goldfinch\Component\Blog\Models\Nest\BlogCategory;
 
-class BlogBlock extends BaseElement
+class BlogBlock extends BlockElement
 {
-    use FielderTrait, Millable;
-
     private static $table_name = 'BlogBlock';
     private static $singular_name = 'Blog';
     private static $plural_name = 'Blog';
@@ -23,11 +18,6 @@ class BlogBlock extends BaseElement
     private static $inline_editable = false;
     private static $description = '';
     private static $icon = 'font-icon-news';
-
-    public function fielder(Fielder $fielder): void
-    {
-        // ..
-    }
 
     public function Items()
     {
@@ -42,17 +32,5 @@ class BlogBlock extends BaseElement
     public function Tags()
     {
         return BlogTag::get();
-    }
-
-    public function getSummary()
-    {
-        return $this->getDescription();
-    }
-
-    public function getType()
-    {
-        $default = $this->i18n_singular_name() ?: 'Block';
-
-        return _t(__CLASS__ . '.BlockType', $default);
     }
 }
